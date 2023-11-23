91e8d13c179eb8216cd964ff4b7df2d8dcd149dc
REI <anhbao1809@gmail.com>
2023-11-23 15:54:54 +0700

    first commit

---
+++ HomePage/homepage.js
@@ -0,0 +1,105 @@
+header2 = document.querySelector('.header .header2')
+topbtn = document.querySelector('.to-the-top')
+window.onscroll = () =>{
+    if(window.scrollY > 80){
+        header2.classList.add('active');
+        topbtn.classList.add('active');
+    }else{
+        header2.classList.remove('active');
+        topbtn.classList.remove('active');
+    }
+}
+
+window.onload = () =>{
+    if(window.scrollY > 80){
+        header2.classList.add('active');
+        topbtn.classList.add('active');
+    }else{
+        header2.classList.remove('active');
+        topbtn.classList.remove('active');
+    }
+}
+
+var swiper = new Swiper(".catalogs", {
+    loop: true,
+    autoplay: {
+        delay: 9000,
+        dispableOnInteraction: false,
+    },
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1385: {
+          slidesPerView: 3,
+        },
+      },
+    spaceBetween: 30,
+})
+
+var swiper = new Swiper(".authors-slider", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+    0: {
+        slidesPerView: 1,
+        },
+        1100: {
+        slidesPerView: 2
+        },
+        1170: {
+        slidesPerView: 3,
+        },
+        1385: {
+        slidesPerView: 4,
+        },
+      },
+      grabCursor: true,
+    });
+
+var swiper = new Swiper(".new-slider", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+        slidesPerView: 1,
+        },
+        700: {
+        slidesPerView: 2
+        },
+        1170: {
+        slidesPerView: 3,
+        },
+        1385: {
+        slidesPerView: 4,
+        },
+    },
+    grabCursor: true,
+    spaceBetween: 30,
+});
+
+var swiper = new Swiper(".cooperators-slider", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 2,
+        },
+        1328: {
+        slidesPerView: 3,
+        },
+        1385: {
+        slidesPerView: 4,
+        },
+    },
+    grabCursor: true,
+});
