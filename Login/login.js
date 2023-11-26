6042d448c89979fc940537b29c333900b477e4d2
Penguin <21521046@gm.uit.edu.vn>
2023-11-26 14:44:50 +0700

    login/register
    JS

--- Login/login.js
+++ Login/login.js
@@ -19,3 +19,28 @@ window.onload = () =>{
         topbtn.classList.remove('active');
     }
 }
+const url = "http://localhost:3000/account"
+fetch(url)
+.then(response => response.json())
+.then(data =>{
+        localStorage.setItem("Account",JSON.stringify(data));
+})
+const INPUT = document.getElementById('form');
+INPUT.addEventListener("submit",(e) => {
+    e.preventDefault();
+     const acc=localStorage.getItem("Account");
+     const account = JSON.parse(acc);
+     const username = document.getElementById("username").value;
+     const password = document.getElementById("password").value;
+     let k=1;
+    account.forEach(item =>{
+        if(item.username == username && item.password==password){
+            alert("dang nhap thanh cong");
+            k=0;
+            window.location.href="../HomePage/homepage.html";
+        }
+    })
+    if(k){
+        alert("thong tin dang nhap chua chinh xac");
+    }
+ })
--- Register/register.js
+++ Register/register.js
@@ -19,3 +19,40 @@ window.onload = () =>{
         topbtn.classList.remove('active');
     }
 }
+const url = "http://localhost:3000/account"
+fetch(url)
+.then(response => response.json())
+.then(data =>{
+        localStorage.setItem("Account",JSON.stringify(data));
+})
+function create(data){
+    fetch(url,{
+        method : "POST",
+        headers: {
+            "Content-Type": "application/json",
+          },
+        body : JSON.stringify(data)
+    }).then(function(response){
+        response.json();
+    })
+    alert("dang ki thanh cong");
+}
+const INPUT = document.getElementById("form");
+INPUT.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const email = document.getElementById("email").value;
+    if (username ==="" || password==="" || email===""){
+        alert('vui long nhap day du thong tin ');
+    }
+    else{
+        var  Account ={
+            username:username,
+            email : email,
+            password:password
+        }
+        create(Account);
+    }
+    
+})
