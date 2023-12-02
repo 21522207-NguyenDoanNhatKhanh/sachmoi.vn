7558ffffaa0c1e976e9fda17d12efd61ce13bd10
NGUYEN TAN PHAT <21521260@gm.uit.edu.vn>
2023-12-02 21:13:26 +0700

    add more things on db

--- includes/header.php
+++ includes/header.php
@@ -21,36 +21,36 @@
                 <li>
                   <a href="category.php?id=4">Văn học Việt Nam</a>
                   <ul class="submenu2">
-                    <li><a href="category.php?id=4">Tiểu thuyết</a></li>
-                    <li><a href="category.php?id=4">Truyện ngắn</a></li>
-                    <li><a href="category.php?id=4">Thơ</a></li>     
+                    <li><a href="category.php?id=5">Tiểu thuyết</a></li>
+                    <li><a href="category.php?id=6">Truyện ngắn</a></li>
+                    <li><a href="category.php?id=7">Thơ</a></li>     
                   </ul>
                 </li>
                 <li>
                   <a href="category.php?id=1">Văn học Nước Ngoài</a>
                   <ul class="submenu2">
-                    <li><a href="">Đương đại</a></li>
-                    <li><a href="">Lãng mạn</a></li>
-                    <li><a href="">Trinh thám / Kinh dị</a></li>
-                    <li><a href="">Light novel</a></li>
-                    <li><a href="">Khoa học</a></li>
+                    <li><a href="category.php?id=8">Đương đại</a></li>
+                    <li><a href="category.php?id=9">Lãng mạn</a></li>
+                    <li><a href="category.php?id=10">Trinh thám / Kinh dị</a></li>
+                    <li><a href="category.php?id=11">Light novel</a></li>
+                    <li><a href="category.php?id=12">Khoa học</a></li>
                   </ul>
                 </li>
                 <li>
                   <a href="category.php?id=2">Sách tham khảo</a>
                   <ul class="submenu2">
-                    <li><a href="">Tiếng Anh</a></li>
-                    <li><a href="">Tiếng Nhật</a></li>
-                    <li><a href="">Tiếng Trung</a></li>
-                    <li><a href="">Tiếng Pháp</a></li>
+                    <li><a href="category.php?id=13">Tiếng Anh</a></li>
+                    <li><a href="category.php?id=14">Tiếng Nhật</a></li>
+                    <li><a href="category.php?id=15">Tiếng Trung</a></li>
+                    <li><a href="category.php?id=16">Tiếng Pháp</a></li>
                   </ul>
                 </li>
                 <li>
                   <a href="category.php?id=3">Sách thiếu nhi</a>
                   <ul class="submenu2">
-                    <li><a href="">Sách ảnh</a></li>
-                    <li><a href="">Sách nói</a></li>
-                    <li><a href="">Sách tô màu</a></li>
+                    <li><a href="category.php?id=17">Sách ảnh</a></li>
+                    <li><a href="category.php?id=18">Sách nói</a></li>
+                    <li><a href="category.php?id=19">Sách tô màu</a></li>
                   </ul>
                 </li>
                 <li>
