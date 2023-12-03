4ac321efa82f6dce87c88c727d01897308353834
NGUYEN TAN PHAT <21521260@gm.uit.edu.vn>
2023-12-03 23:22:49 +0700

    nothing

--- view_orders.php
+++ view_orders.php
@@ -15,45 +15,44 @@ include 'includes/header.php';
 </head>
 
 <body>
-    <div class="container">
-        <?php
-            if (isset($_SESSION['user_id'])) {
-                $user_id = $_SESSION['user_id'];
+    <?php
+        if (isset($_SESSION['user_id'])) {
+            $user_id = $_SESSION['user_id'];
 
-                $user_info = get_user_info($user_id);
+            $user_info = get_user_info($user_id);
 
-                if ($user_info) {
-                    $username = $user_info['username'];
+            if ($user_info) {
+                $username = $user_info['username'];
+                echo '<div class="container">';
+                echo '<h2>Các đơn hàng của bạn (' . $username . ')</h2>';
 
-                    echo '<h2>Các đơn hàng của bạn (' . $username . ')</h2>';
+                $orders = get_user_orders($user_id);
 
-                    $orders = get_user_orders($user_id);
-
-                    if ($orders) {
-                        foreach ($orders as $order) {
-                            echo '<div class="box">';
-                                echo '<ul>';
-                                echo '<li>Mã đơn hàng: ' . $order['order_id'] . '</li>';
-                                echo '<li>Tổng số tiền: ' . $order['total_amount'] . '</li>';
-                                echo '<li>Trạng thái vận chuyển: ' . $order['shipping_status'] . '</li>';
-                                echo '<li>Trạng thái thanh toán: ' . $order['payment_status'] . '</li>';
-                                echo '<li>Ngày đặt hàng: ' . $order['order_date'] . '</li>';
-                                echo '</ul>';
-                            echo '</div>';
-                        }
-                        
-                    } else {
-                        echo '<p>Bạn chưa có đơn hàng nào.</p>';
+                if ($orders) {
+                    foreach ($orders as $order) {
+                        echo '<div class="box">';
+                            echo '<ul>';
+                            echo '<li>Mã đơn hàng: ' . $order['order_id'] . '</li>';
+                            echo '<li>Tổng số tiền: ' . $order['total_amount'] . '</li>';
+                            echo '<li>Trạng thái vận chuyển: ' . $order['shipping_status'] . '</li>';
+                            echo '<li>Trạng thái thanh toán: ' . $order['payment_status'] . '</li>';
+                            echo '<li>Ngày đặt hàng: ' . $order['order_date'] . '</li>';
+                            echo '</ul>';
+                        echo '</div>';
                     }
+                    
                 } else {
-                    echo 'Lỗi: Không thể lấy thông tin người dùng.';
+                    echo '<p>Bạn chưa có đơn hàng nào.</p>';
                 }
+                echo '</div>';
             } else {
-                header('Location: login.php');
-                exit();
-        }
-        include 'includes/footer.php';
-        ?>
-    </div>
+                echo 'Lỗi: Không thể lấy thông tin người dùng.';
+            }
+        } else {
+            header('Location: login.php');
+            exit();
+    }
+    include 'includes/footer.php';
+    ?>
 </body>
 </html>
